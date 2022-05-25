7e80849a1d82ae0d678873cb1b2ea230ae027271
Tien Dao <tiendao5240@gmail.com>
2022-05-24 22:29:53 -0400

    Basic group

--- uiCollectionViewCompositionalLayout/ViewController.swift
+++ uiCollectionViewCompositionalLayout/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.collectionViewLayout = configLayout()
+        configDataSource()
     }
     
     private func configLayout() -> UICollectionViewCompositionalLayout {
@@ -32,19 +33,20 @@ class ViewController: UIViewController {
         item.contentInsets = NSDirectionalEdgeInsets(top:5, leading: 5, bottom: 5, trailing: 5)
         
         //Configurate group
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalWidth(0.5))
+        //Group size relative values based on the screen size
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5))
 //        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
         
         //basic grid
-        // let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: item, count: 2)
+         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
         
         //Vertical layout
         // let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: item, count: 3)
         
         //sub group
-        let subgroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(0.5)), subitem: item, count: 2)
-        
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: subgroup, count: 2)
+//        let subgroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(0.5)), subitem: item, count: 2)
+//
+//        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: subgroup, count: 2)
         
         let section = NSCollectionLayoutSection(group: group)
         //Added spacing between the section
@@ -54,7 +56,32 @@ class ViewController: UIViewController {
         
     }
     
-
+    func configDataSource() {
+        self.dataSource = UICollectionViewDiffableDataSource<section, Int>(collectionView: self.collectionView) { (collectionView, indexPath, number) -> UICollectionViewCell? in
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? ItemsCollectionViewCell else {
+                fatalError("Index Is invalid")
+            }
+            
+            cell.itemLabel.text = "(\(number)"
+            cell.backgroundColor = self.randomColor()
+            return cell
+            
+        }
+        
+        var initSnapShot = NSDiffableDataSourceSnapshot<section,Int>()
+        initSnapShot.appendSections([.main])
+        initSnapShot.appendItems(Array(1...12))
+        dataSource.apply(initSnapShot)
+    }
+    
+    //custom function to generate a random UIColor
+    func randomColor() -> UIColor{
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
     
 
 
