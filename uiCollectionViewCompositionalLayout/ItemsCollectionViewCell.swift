bdf30cfa633733977b47180437a24c16ad913448
Tien Dao <tiendao5240@gmail.com>
2022-05-24 22:08:43 -0400

    Added Class for items

---
+++ uiCollectionViewCompositionalLayout/ItemsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ItemsCollectionViewCell.swift
+//  uiCollectionViewCompositionalLayout
+//
+//  Created by Tien Dao on 2022-05-24.
+//
+
+import UIKit
+
+class ItemsCollectionViewCell: UICollectionViewCell {
+    
+}
