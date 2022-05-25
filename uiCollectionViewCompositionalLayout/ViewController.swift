1282b3368d7533e8160171d6e82c5887d0005e98
Tien Dao <tiendao5240@gmail.com>
2022-05-24 20:41:42 -0400

    Added enum and dataSource

--- uiCollectionViewCompositionalLayout/ViewController.swift
+++ uiCollectionViewCompositionalLayout/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum section {
+        case main
+    }
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var dataSource: UICollectionViewDiffableDataSource<section,Int>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
